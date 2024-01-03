690af1034b0de885d06b1ce68663b87c2a26421c
ashish0526 <jethvaniashish@gmail.com>
2024-01-03 19:51:08 +0530

    Added changes

---
+++ mssc-beet-service/src/main/java/guru/springFramework/msscbeetservice/MsscBeetServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springFramework.msscbeetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeetServiceApplication.class, args);
+	}
+
+}
